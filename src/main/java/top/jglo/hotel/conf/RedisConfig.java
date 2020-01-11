be6f6468634dd1f0ae9d9ea1883d66cb6c161553
金戈宁 <jingening@gmail.com>
2020-01-11 22:34:03 +0800

    1

--- src/main/java/top/jglo/hotel/conf/RedisConfig.java
+++ src/main/java/top/jglo/hotel/conf/RedisConfig.java
@@ -1,32 +1,32 @@
-package top.jglo.hotel.conf;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import top.jglo.hotel.coreinterceptor.CoreInterceptor;
-
-/**
- * @author Gkirito
- */
-@Configuration
-public class RedisConfig extends WebMvcConfigurerAdapter {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName("39.106.56.132");
-        jedisConnectionFactory.setPort(6379);
-        jedisConnectionFactory.setPassword("123456");
-        return jedisConnectionFactory;
-    }
-    @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
-        return stringRedisTemplate;
-    }
-
-}
+//package top.jglo.hotel.conf;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import top.jglo.hotel.coreinterceptor.CoreInterceptor;
+//
+///**
+// * @author Gkirito
+// */
+//@Configuration
+//public class RedisConfig extends WebMvcConfigurerAdapter {
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+//        jedisConnectionFactory.setHostName("39.106.56.132");
+//        jedisConnectionFactory.setPort(6379);
+//        jedisConnectionFactory.setPassword("123456");
+//        return jedisConnectionFactory;
+//    }
+//    @Bean
+//    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+//        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+//        return stringRedisTemplate;
+//    }
+//
+//}
